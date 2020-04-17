9339c0dcc92a6666b34554ddaf8c5370239f541f
global-event-2762 <rosalinschlosser@gmail.com>
2020-04-17 20:01:29 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+  attr_accessor :roster
+  attr_reader :school_name
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {};
+  end
+
+  def add_student(student_name, grade)
+    if @roster[grade] == nil
+      @roster[grade] = []
+      @roster[grade] << student_name
+    else
+      @roster[grade] << student_name
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    sorted_roster = {}
+    @roster.each {|grade, students| sorted_roster[grade] = students.sort}
+    sorted_roster
+  end
+end
