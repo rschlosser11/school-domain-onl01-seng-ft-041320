ac241543e76c5ed4f16ab8d6c4f32bb0330810f6
global-event-2762 <rosalinschlosser@gmail.com>
2020-04-17 19:27:40 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,6 +1,12 @@
 class School
   ROSTER = {};
   def add_student(student_name, grade)
-    ROSTER[grade] = student_name
+    if ROSTER["#{grade}"] == nil 
+      ROSTER["#{grade}"] = []
+      ROSTER["#{grade}"] << student_name
+    else
+      ROSTER["#{grade}"] << student_name
+    end
   end
+  ROSTER
 end
